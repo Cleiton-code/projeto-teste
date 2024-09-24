5f1bbbaef7d227cc7ae677c4dd4f4d4869dd8a26
cleiton valentim <cleiton_valentim@hotmail.com.br>
2024-09-24 10:09:15 -0300

    aula 24/09

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit{
+
+  constructor(private router: Router,
+     private activeRoute: ActivatedRoute,
+     private service: ProductService ){
+
+  }
+  ngOnInit() {
+    const id = Number(this.activeRoute.snapshot.paramMap.get("id"));
+    this.loadProduct(id);
+  }
+  loadProduct(id: number) {
+    this.service.getProductById(id).subscribe({
+      next: data => alert(data.name)
+    });
+
+  }
 
 }
